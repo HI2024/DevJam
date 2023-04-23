308e0348a5d3fdb03278bbb3578294e07e49e366
Code-Parth <softsparths@gmail.com>
2023-04-23 12:13:42 +0530

    Connect wallet function

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { ethers } from 'ethers';
 
 function App() {
+  const [walletAddress, setWalletAddress] = useState("");
+
+  async function requestAccount() {
+    console.log('Requesting account...');
+
+    if (window.ethereum) {
+      console.log('detected');
+
+      try {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        setWalletAddress(accounts[0]);
+      } catch (error) {
+        console.log('Error connecting...');
+      }
+
+    } else {
+      alert('Meta Mask not detected');
+    }
+  }
+
+  // async function connectWallet() {
+  //   if (typeof window.ethereum !== 'undefined') {
+  //     await requestAccount();
+
+  //     const provider = new ethers.providers.Web3Provider(window.ethereum);
+  //   }
+  // }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={requestAccount}>Request Account</button>
+        <h3>Wallet Address: {walletAddress}</h3>
       </header>
     </div>
   );
