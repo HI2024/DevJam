b10be0ae87020612a4f4ecdf2d367e6e7619b14b
SarthakBhave <kanaiya2182@gmail.com>
2023-04-29 20:19:07 +0530

    pr

--- src/components/Metamask/Connect-Metamask.js
+++ src/components/Metamask/Connect-Metamask.js
@@ -1,47 +1,48 @@
-import { useState } from 'react';
+import { useState } from "react";
 // import { ethers } from 'ethers';
 
 const style = {
-    button: `text-white`,
-}
+  button: `text-white``backgroundcolor-blue`,
+};
 
 function ConnectMetamask() {
-    const [walletAddress, setWalletAddress] = useState("");
+  const [walletAddress, setWalletAddress] = useState("");
 
-    async function requestAccount() {
-        console.log('Requesting account...');
+  async function requestAccount() {
+    console.log("Requesting account...");
 
-        if (window.ethereum) {
-            console.log('detected');
+    if (window.ethereum) {
+      console.log("detected");
 
-            try {
-                const accounts = await window.ethereum.request({
-                    method: "eth_requestAccounts",
-                });
-                setWalletAddress(accounts[0]);
-            } catch (error) {
-                console.log('Error connecting...');
-            }
-
-        } else {
-            alert('Meta Mask not detected');
-        }
+      try {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        setWalletAddress(accounts[0]);
+      } catch (error) {
+        console.log("Error connecting...");
+      }
+    } else {
+      alert("Meta Mask not detected");
     }
+  }
 
-    // async function connectWallet() {
-    //   if (typeof window.ethereum !== 'undefined') {
-    //     await requestAccount();
+  // async function connectWallet() {
+  //   if (typeof window.ethereum !== 'undefined') {
+  //     await requestAccount();
 
-    //     const provider = new ethers.providers.Web3Provider(window.ethereum);
-    //   }
-    // }
+  //     const provider = new ethers.providers.Web3Provider(window.ethereum);
+  //   }
+  // }
 
-    return (
-        <div>
-            <button onClick={requestAccount} className={style.button}>Connect Metamask</button>
-            {/* <h3>Wallet Address: {walletAddress}</h3> */}
-        </div>
-    )
+  return (
+    <div>
+      <button onClick={requestAccount} className={style.button}>
+        Connect Metamask
+      </button>
+      {/* <h3>Wallet Address: {walletAddress}</h3> */}
+    </div>
+  );
 }
 
 export default ConnectMetamask;
